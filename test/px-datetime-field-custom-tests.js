99e67d2901838391cc3afbfe4b5c2b9764554653
katemenkhaus <katherine.menkhaus@ge.com>
2018-03-01 15:51:22 -0800

    trying clean

--- test/px-datetime-field-custom-tests.js
+++ test/px-datetime-field-custom-tests.js
@@ -1237,23 +1237,3 @@ suite('Invalid', function() {
     });
   });
 });
-
-/**
- * FOCUS
- */
-suite('Focus', function() {
-  let field;
-
-  setup(function(done) {
-    field = fixture('px_datetime_field');
-    flush(()=>{
-      done();
-    });
-  });
-
-  test('Host should propagate focus to the first entry', function() {
-    var entries = Polymer.dom(field.root).querySelectorAll('px-datetime-entry');
-    field.focus()
-    assert.equal(entries[0], Polymer.dom(field.root).activeElement);
-  });
-});
